c166bc72471a50fd263641d701e28248b9e47f8f
Alexander Zorov <noexile@gmail.com>
2016-04-07 11:17:25 +0300

    - added CleanWastePileServlet

---
+++ src/controller/CleanWastepileServlet.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CleanWastepileServlet")
+public class CleanWastepileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("testche");
+	}
+
+}
