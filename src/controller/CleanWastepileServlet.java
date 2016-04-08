6653baea2f7f9b2fd6f46fd0b25545633a145c85
Alexander Zorov <noexile@gmail.com>
2016-04-08 12:29:46 +0300

    - updated the CleanWastepileServlet. Added needed survivor to be selected with constraint for him to be in the colony

--- src/controller/CleanWastepileServlet.java
+++ src/controller/CleanWastepileServlet.java
@@ -1,12 +1,15 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.character.Survivor;
 import model.location.GameMap;
 import model.user.Player;
 
@@ -20,10 +23,25 @@ public class CleanWastepileServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String survivorName = request.getParameter("selected_survivor");
 		String dice = request.getParameter("rolled_dice");
 
 		GameMap map = (GameMap) request.getSession().getAttribute("map");
 		Player player = (Player) request.getSession().getAttribute("player");
+		List<Survivor> playerSurvivors = player.getSurvivors();
+		Survivor pickedSurvivor = getSurvivor(survivorName ,playerSurvivors);
+		
+		if (survivorName == null) {
+			request.getSession().setAttribute("cleanWastepileError", "You must select a survivor to clean the wastepile!");
+			request.getRequestDispatcher("boardgame.jsp").forward(request, response);
+			return;
+		}
+		
+		if (!pickedSurvivor.getCurrentLocation().equals(map.getColony())) {
+			request.getSession().setAttribute("cleanWastepileError", "The survivor must be in the colony to clean the wastepile!");
+			request.getRequestDispatcher("boardgame.jsp").forward(request, response);
+			return;
+		}
 		
 		if (dice.trim().isEmpty()) {
 			request.getSession().setAttribute("cleanWastepileError", "No dice selected for cleaning wastelipe!");
@@ -48,6 +66,16 @@ public class CleanWastepileServlet extends HttpServlet {
 		map.getColony().clearWastePile();
 		
 		request.getRequestDispatcher("boardgame.jsp").forward(request, response);				
+	}
+
+	private Survivor getSurvivor(String survivorName, List<Survivor> playerSurvivors) {
+			
+		for (int i = 0; i < playerSurvivors.size(); i++) {
+			if (playerSurvivors.get(i).getName().equals(survivorName)) {
+				return playerSurvivors.get(i);
+			}
+		}
+		return null;
 	}
 
 }
