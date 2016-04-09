aaeba3a83c475dde6bd6de389d2189944b8412bb
Alexander Zorov <noexile@gmail.com>
2016-04-09 22:38:24 +0300

    - updated and implemented logic for 4 Secret Objectives for the game

--- src/model/objective/secretobjectives/ANewDestination.java
+++ src/model/objective/secretobjectives/ANewDestination.java
@@ -1,13 +1,40 @@
 package model.objective.secretobjectives;
 
+import model.card.Item;
 import model.objective.SecretObjectiveGoal;
+import model.user.Player;
 
 public class ANewDestination extends SecretObjectiveGoal {
 	
 	public static final int FUEL_NEEDED = 3;
 	
-	
+	private static ANewDestination instance = null;
+		
 	public ANewDestination() {
-		super("A New Destination", "resources/a_new_destination.png");
+		super("A New Destination", Item.Type.FUEL.toString(), "resources/a_new_destination.png");
+	}
+	
+	public static ANewDestination getInstance(){
+		if(instance == null)
+			instance = new ANewDestination();
+		return instance;
+	}
+
+	
+	@Override
+	public boolean meetRequirements(Player player) {
+		int counter = 0;
+		
+		for (int i = 0; i < player.getPlayerItems().size(); i++) {
+			if (player.getPlayerItems().get(i).getType().equalsIgnoreCase(this.getType())) {
+				counter++;
+			}
+		}
+		
+		if(counter >= FUEL_NEEDED) {
+			return true;
+		}
+		
+		return false;
 	}
 }
--- src/model/objective/secretobjectives/Hunger.java
+++ src/model/objective/secretobjectives/Hunger.java
@@ -1,14 +1,41 @@
 package model.objective.secretobjectives;
 
+import model.card.Item;
 import model.objective.SecretObjectiveGoal;
+import model.user.Player;
 
 public class Hunger extends SecretObjectiveGoal {
 	
 	public static final int FOOD_NEEDED = 3;
-	
+
+	private static Hunger instance = null;
 
 	public Hunger() {
-		super("Hunger", "resources/hunger.png");
+		super("Hunger", Item.Type.FOOD.toString(), "resources/hunger.png");
+	}
+
+	public static Hunger getInstance(){
+		if(instance == null)
+			instance = new Hunger();
+		return instance;
+	}
+
+	
+	@Override
+	public boolean meetRequirements(Player player) {
+		int counter = 0;
+		
+		for (int i = 0; i < player.getPlayerItems().size(); i++) {
+			if (player.getPlayerItems().get(i).getType().equalsIgnoreCase(this.getType())) {
+				counter++;
+			}
+		}
+		
+		if(counter >= FOOD_NEEDED) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 }
