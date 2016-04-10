16dc57ce319298682fc4e251a4e2e008304e15e7
Kiril Petrov <kiril.petrov94@gmail.com>
2016-04-10 19:04:14 +0300

    Converting pass to md5 when register in Map

--- src/model/db/DBUserDao.java
+++ src/model/db/DBUserDao.java
@@ -1,5 +1,6 @@
 package model.db;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -96,6 +97,12 @@ public class DBUserDao implements IUserDAO {
 	}
 	
 	private void addUserInMap(User u){
+		try {
+			u.setPassword(convertToMd5(u.getPassword()));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.allUsers.put(u.getUsername(), u);
 	}
 	
@@ -148,5 +155,20 @@ public class DBUserDao implements IUserDAO {
 			return null;
 		}
 	}
+	
+	private static String convertToMd5(final String md5) throws UnsupportedEncodingException {
+        StringBuffer sb=null;
+        try {
+            final java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            final byte[] array = md.digest(md5.getBytes("UTF-8"));
+            sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (final java.security.NoSuchAlgorithmException e) {
+        }
+        return sb.toString();
+    }
 
 }
