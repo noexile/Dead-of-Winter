599c8a24b361e8c3df2aa0200ec559440f0f3e2f
Alexander Zorov <noexile@gmail.com>
2016-04-10 14:00:10 +0300

    - food was bugged

---
+++ src/model/card/playercards/Food.java
@@ -0,0 +1,13 @@
+package model.card.playercards;
+
+import model.card.Item;
+import model.card.PlayerCard;
+
+public class Food extends PlayerCard {
+
+	
+	public Food() {
+		super("Food", Item.Type.FOOD, "resources/food1.png");
+	}
+
+}
